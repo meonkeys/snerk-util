45aa9536a50bfc87f0e2eb58812745bd468f877c
Adam Monsen <haircut@gmail.com>
2012-04-03 21:46:37 -0700

    Add license (MIT Expat).

--- php/lib/TwoBulb/ArrayUtilTest.php
+++ php/lib/TwoBulb/ArrayUtilTest.php
@@ -1,5 +1,8 @@
 <?php
 
+# Copyright (C) 2012 Adam Monsen <haircut@gmail.com>
+# License: MIT License. See end of script for more license information.
+
 namespace TwoBulb;
 
 require_once 'ArrayUtil.php';
@@ -47,3 +50,24 @@ class ArrayUtilTest extends \PHPUnit_Framework_TestCase {
   }
 
 }
+
+# "MIT" (Expat) license:
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
